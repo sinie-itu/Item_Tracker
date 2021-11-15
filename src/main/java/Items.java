3869926f84d43d4bc68887f954bc6831e5cbd8c0
Simon Nielsen <sinie@itu.dk>
2021-11-15 20:24:41 +0100

    fields and constructor of Items
    added relevant fields and initialize them in the constructor

---
+++ src/main/java/Items.java
@@ -0,0 +1,11 @@
+public class Items {
+    String name;
+    String type;
+    int price;
+
+    public Items(String name, String type, int price) {
+        this.name = name;
+        this.type = type;
+        this.price = price;
+    }
+}
